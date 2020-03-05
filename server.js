30f7932bc9ac3b9518420c343d2e5bac8adbec27
sophiet12 <57049410+sophiet12@users.noreply.github.com>
2020-03-05 16:12:12 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcript = require('bcrypt-nodejs');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const database = {
+	users: [
+		{
+			id:'123',
+			name: 'John',
+			email: 'john@gmail.com',
+			password:'cookies',
+			entries:0, 
+			joined:new Date()
+		}, 
+		{
+			id:'124',
+			name: 'Sally',
+			email: 'sally@gmail.com',
+			password:'bananas',
+			entries:0,
+			joined:new Date()
+		}
+	]
+}
+app.get('/', (req,res)=>{
+	res.send(database.users);})
+
+app.post('/signin', (req,res)=>{
+	if (req.body.email === database.users[0].email && req.body.password === database.users[0].password)
+		{res.json(database.users[0]);}
+	else 
+	{res.status(400).json('no user find');}
+	 })
+
+app.post('/register', (req,res)=>{ 
+	const {email,name,password} = req.body;
+	database.users.push({
+
+			id:'125',
+			name: name,
+			email: email,
+			password: password,
+			entries:0,
+			joined:new Date()
+	})
+		res.json(database.users[database.users.length-1]);
+
+})
+
+app.get('/profile/:id' , (req,res) => {
+	let found = false;
+	const {id} = req.params;
+	database.users.forEach(user => {
+		if (user.id === id) {
+			found = true;
+			return res.json(user);
+		}
+	})
+	if (!found)
+		res.status(404).json('no such user');
+})
+
+app.put('/image', (req,res) => {
+	let found = false;
+	const {id} = req.body;
+	database.users.forEach(user => {
+		if (user.id === id) {
+			found = true;
+			user.entries++;
+			return res.json(user);
+		}
+	})
+	if (!found)
+		res.status(404).json('no such user');
+})
+
+
+app.listen(4000, ()=> {
+	console.log('app is running');
+});
